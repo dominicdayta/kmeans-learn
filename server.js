63acb906c78eccc0be159ca8ff1198a253727d03
dominicdayta <dbdayta@up.edu.ph>
2022-05-06 19:19:36 +0800

    Prepare for launch

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 
 const Students = require('./models/students');
 
-mongoose.connect(process.env['DATABASE_URL'], { useNewUrlParser: true });
+mongoose.connect(process.env['DATABASE_URL2'], { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
